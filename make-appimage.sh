d6e164e65888b2e36c6d76ac0819d6847c44cd5f
fiftydinar <fiftydinar@users.noreply.github.com>
2026-09-09 11:59:51 +0200

    build yt-dlp from source and use quickjs runtime
    Arch (and especially Arch Linux ARM) ships an outdated yt-dlp, so build it
    from the archlinux PKGBUILD like yt-dlp-AppImage does:
    
    - build quickjs from source (yt-dlp needs a JS runtime for site challenges)
    - build yt-dlp-ejs and yt-dlp via make-aur-package, removing the deno
      dependency from the PKGBUILD (we use quickjs instead)
    - explicitly deploy /usr/bin/yt-dlp and /usr/bin/qjs in the AppImage

--- make-appimage.sh
+++ make-appimage.sh
@@ -20,8 +20,9 @@ export USE_HOST_DRIVERS_EXPERIMENTAL=1
 # Deploy dependencies
 clapper_dir=$(echo /usr/lib/clapper*)
 quick-sharun /usr/bin/clapper "$clapper_dir" \
+             /usr/bin/yt-dlp \
              /usr/bin/qjs \
-             /usr/lib/libpeas-2/loaders/*
+             /usr/lib/libpeas-2/loaders
 
 echo "CLAPPER_SINK_IMPORTER_PATH=\${SHARUN_DIR}/lib/${clapper_dir##*/}/gst/plugin/importers" >> ./AppDir/.env
 echo "CLAPPER_ENHANCERS_PATH=\${SHARUN_DIR}/lib/${clapper_dir##*/}/enhancers" >> ./AppDir/.env
